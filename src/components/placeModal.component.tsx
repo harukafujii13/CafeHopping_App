3cf91b2569535e3c0ae60f23a1099cb0c3a5524c
Haruka <hfujii1013@gmail.com>
2023-07-15 17:06:37 -0700

    feat: change responsive

--- src/components/placeModal.component.tsx
+++ src/components/placeModal.component.tsx
@@ -9,50 +9,45 @@ interface ModalProps {
 const PlaceModal: FC<ModalProps> = ({ isOpen, closeModal, place }) => {
   return (
     <div
-      className={`fixed z-10 top-10 inset-0 overflow-y-auto ${
+      className={`fixed z-50 inset-0 overflow-y-auto ${
         isOpen ? '' : 'hidden'
       }`}>
-      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center">
         <div
-          className="fixed inset-0 transition-opacity"
-          aria-hidden="true">
-          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
-
+          className="fixed inset-0 bg-gray-800 opacity-50 transition-opacity"
+          aria-hidden="true"></div>
         <div
-          className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full text-primary-gray"
+          className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all w-4/5 sm:w-3/4 md:w-1/2 lg:w-2/5"
           role="dialog"
           aria-modal="true"
           aria-labelledby="modal-headline">
-          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <div className="sm:flex sm:items-start  justify-center">
+          <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start justify-center">
               <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                 <h3
-                  className="text-xl leading-6 font-bold font-inter text-primary-gray flex justify-center"
+                  className="text-2xl leading-6 font-bold text-primary-gray font-inter mb-4"
                   id="modal-headline">
                   {place?.name}
                 </h3>
-                <div className="mt-2">
-                  {place?.opening_hours && (
-                    <div>
-                      <h4 className="font-medium text-base font-inter text-primary-yellow">
-                        Opening Hours
-                      </h4>
-                      <ul className="font-inter font-normal text-sm">
-                        {place.opening_hours.weekday_text.map((day, index) => (
-                          <li key={index}>{day}</li>
-                        ))}
-                      </ul>
-                    </div>
-                  )}
-                </div>
+                {place?.opening_hours && (
+                  <div className="text-left">
+                    <h4 className="font-medium text-lg text-primary-yellow mb-2 font-inter ">
+                      Opening Hours
+                    </h4>
+                    <ul className="list-disc list-inside space-y-2 text-primary-gray text-md font-inter">
+                      {place.opening_hours.weekday_text.map((day, index) => (
+                        <li key={index}>{day}</li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
               </div>
             </div>
           </div>
           <div className="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
             <button
               type="button"
-              className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-primary-coral text-x font-bold text-white hover:bg-primary-rose focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#95b1a8] sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              className="w-full inline-flex justify-center rounded-md px-4 py-2 bg-primary-coral text-base font-medium text-white font-inter hover:bg-primary-rose focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#b9cbc6] sm:mt-0 sm:w-auto sm:text-sm"
               onClick={closeModal}>
               Close
             </button>
