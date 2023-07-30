0c86f4cd897f2980135f397a97702676d7aea267
Haruka <hfujii1013@gmail.com>
2023-07-29 21:18:00 -0700

    feat: add Login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,13 @@
-import {
-  LoginButton,
-  LogoutButton,
-  RegisterButton,
-  ProfileButton,
-} from '@/components/buttons.component';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
-import { User } from '@/components/user.component';
+import LoginPage from './login/page';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
-    <main className="flex justify-center items-center h-[70vh]">
+    <main>
       <div>
-        <LoginButton />
-        <RegisterButton />
-        <ProfileButton />
-        <LogoutButton />
-        <h1 className="text-7xl font-bold mb-3">Server Session</h1>
-        <code>{JSON.stringify(session)}</code>
-        <User />
+        <LoginPage />
       </div>
     </main>
   );
