ada3e92b01b9336d9ac0e76b7c955d8a5a640e07
Haruka <hfujii1013@gmail.com>
2023-08-02 11:15:17 -0700

    style: add bookmark and fav icons

--- src/components/cafefinder.component.tsx
+++ src/components/cafefinder.component.tsx
@@ -306,22 +306,31 @@ const CafeFinder: FC = () => {
                   )}
                 </div>
                 {/* Add any additional information about the place here */}
-                <button
-                  onClick={() => handleMoreInfo(place)}
-                  className="inline-flex items-center px-3 py-2 text-x font-inter font-bold text-center text-white bg-primary-coral rounded-lg hover:bg-primary-rose focus:ring-4 focus:outline-none focus:ring-[#b9cbc6] dark:bg-[#95b1a8] dark:hover:bg-primary-green dark:focus:ring-[#688d81]">
-                  More info
-                  <svg
-                    aria-hidden="true"
-                    className="w-4 h-4 ml-2 -mr-1"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      fillRule="evenodd"
-                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                      clipRule="evenodd"></path>
-                  </svg>
-                </button>
+                <div className="flex flex-row gap-3 items-center">
+                  <div className="text-primary-gray text-[1.7rem]">
+                    <BsFillBookmarkDashFill />
+                  </div>
+                  <div className="text-primary-gray text-[1.7rem]">
+                    <MdFavorite />
+                  </div>
+
+                  <button
+                    onClick={() => handleMoreInfo(place)}
+                    className="inline-flex items-center px-3 py-2 text-x font-inter font-bold text-center text-white bg-primary-coral rounded-lg hover:bg-primary-rose focus:ring-4 focus:outline-none focus:ring-[#b9cbc6] dark:bg-[#95b1a8] dark:hover:bg-primary-green dark:focus:ring-[#688d81]">
+                    More info
+                    <svg
+                      aria-hidden="true"
+                      className="w-4 h-4 ml-2 -mr-1"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        fillRule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clipRule="evenodd"></path>
+                    </svg>
+                  </button>
+                </div>
               </div>
             </div>
           ))}
