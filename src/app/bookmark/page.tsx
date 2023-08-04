62478603a2e86dfcbcea4311a32461ad2fa64774
Haruka <hfujii1013@gmail.com>
2023-08-04 10:36:04 -0700

    add: Bookmarks component

--- src/app/bookmark/page.tsx
+++ src/app/bookmark/page.tsx
@@ -1,10 +1,12 @@
 import { Navbar } from '@/components/nav.component';
+import Bookmarks from './form';
 
 export default function Bookmark() {
   return (
     <div className="h-[100vh] bg-light-green">
       <Navbar />
       <p>Bookmark page</p>
+      <Bookmarks userId={'2e0db5ee-8d45-44e5-b712-4d070d3d5529'} />
     </div>
   );
 }
