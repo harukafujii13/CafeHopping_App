bff82b8c208adb6b403abe1d30bd6516e5af12e9
Haruka <hfujii1013@gmail.com>
2023-08-09 18:19:25 -0700

    feat: error handler

--- src/components/bookmarkButton.tsx
+++ src/components/bookmarkButton.tsx
@@ -4,24 +4,32 @@ import { BsFillBookmarkDashFill } from 'react-icons/bs';
 import { Place } from '@/components/cafefinder.component';
 
 const BookmarkButton: React.FC<{ place: Place }> = ({ place }) => {
-  const { data } = useSession();
+  const { data: session } = useSession();
 
   async function handleBookmarkClick() {
-    const userId = 'replace-this-with-the-actual-user-id';
+    if (!session) {
+      console.log('User not authenticated');
+      //show a toast later
+      return;
+    }
 
-    const photosArray = place.photos?.map((photo) => photo.getUrl());
+    const userId = session;
+
+    // const photosArray = place.photos?.map((photo) => photo.getUrl());
 
     const response = await fetch('/api/bookmark/create', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ ...place, photosArray }),
+      body: JSON.stringify({ userId, ...place }),
     });
 
     if (!response.ok) {
       // Error handling
       console.error('Failed to bookmark cafe');
+    } else {
+      //show a toast
     }
   }
 
