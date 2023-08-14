d5c04128db526750dfae9d365d8dfda380fc275b
Haruka <hfujii1013@gmail.com>
2023-08-14 13:53:23 -0700

    feat: add force-dynamic

--- src/app/api/bookmark/[userId]/route.ts
+++ src/app/api/bookmark/[userId]/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: Request, context: any) {
   const queryUserId: string = await context.params!.userId;
 
