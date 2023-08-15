0e0aeb325e1287dfeb36116a0bd0fb581d25806a
Haruka <hfujii1013@gmail.com>
2023-08-15 10:13:22 -0700

    style: change bookmark color

--- src/components/bookmark/bookmarkButton.tsx
+++ src/components/bookmark/bookmarkButton.tsx
@@ -14,7 +14,7 @@ const BookmarkButton = ({ place }: { place: Place | BookMarkPlace }) => {
   //Check if cafe is already bookmarked
   const alreadyBookmarked = isBookmarked(place.place_id);
 
-  const bookmarkColor = alreadyBookmarked ? '#004346' : '#6b7280';
+  const bookmarkColor = alreadyBookmarked ? '#F6BD60' : '#6b7280';
 
   async function handleBookmarkClick() {
     if (!session) {
