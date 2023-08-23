7fa61e03833268452e79c0335f2aaaf4893dfb9d
Haruka <hfujii1013@gmail.com>
2023-08-23 15:47:46 -0700

    feat: edit functiom

--- src/app/api/reviewComment/[id]/updateReview/route.ts
+++ src/app/api/reviewComment/[id]/updateReview/route.ts
@@ -1,38 +1,47 @@
-//update review
-
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
-export async function PATCH(req: Request) {
-  // Get user ID from session
-  let userId;
+export async function PATCH(
+  req: Request,
+  { params }: { params: { id: string; content: string } }
+) {
+  // Get the review ID from the provided params
+  const reviewId = params.id;
+
+  // Get data from request body
+  let content;
   try {
-    const session = await getServerSession(authOptions);
-    if (session && session.user) {
-      userId = session.user.id;
-    } else {
-      throw new Error('Session or user not found.');
-    }
-  } catch (error: any) {
+    const body = await req.json();
+    content = body.content;
+  } catch (error) {
+    console.error('Failed to parse request body.', error);
     return new NextResponse(
       JSON.stringify({
         status: 'error',
-        message: error.message || 'Failed to retrieve session.',
+        message: 'Invalid request body.',
       }),
-      { status: 500 }
+      { status: 400 }
     );
   }
 
-  // Get the review ID from the request URL parameter
-  const reviewId = req.url?.split('/').slice(-3)[0];
-
-  // Get data from request body (i.e., the updated review content)
-  const { content } = await req.json();
-
   try {
-    // Find the review by its ID and update the content
+    // Check if review exists and belongs to the current user
+    const existingReview = await prisma.review.findUnique({
+      where: {
+        id: reviewId,
+      },
+    });
+    if (!existingReview || existingReview.userId !== userId) {
+      return NextResponse.json({
+        status: 'error',
+        message:
+          'Review not found or you are not authorized to update this review.',
+      });
+    }
+
+    // Update the review
     const updatedReview = await prisma.review.update({
       where: {
         id: reviewId,
@@ -43,6 +52,7 @@ export async function PATCH(req: Request) {
     });
 
     return NextResponse.json({
+      success: true,
       review: {
         id: updatedReview.id,
         userId: updatedReview.userId,
@@ -51,7 +61,7 @@ export async function PATCH(req: Request) {
       },
     });
   } catch (error: any) {
-    console.log({ error });
+    console.error('Failed to update review.', error);
     return new NextResponse(
       JSON.stringify({
         status: 'error',
--- src/components/reviewComment/reviewInputForm.component.tsx
+++ src/components/reviewComment/reviewInputForm.component.tsx
@@ -1,5 +1,4 @@
-'use client';
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { Place } from '@/components/cafeFinder/cafefinder.component';
 import { BookMarkPlace } from '@/app/bookmark/bookmarkCafe';
@@ -13,15 +12,36 @@ const ReviewInputForm = ({
   setShowReviewFormOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
   const { data: session } = useSession();
-  // console.log('/////data', data);
+
   const [review, setReview] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
   const { isReviewed, fetchReviewsByCafeId, addReview, updateReview } =
     useContext(CafeContext);
 
-  // Check if cafe is already reviewed
   const alreadyReviewed = isReviewed();
-  // console.log('/////alreadyReviewed', alreadyReviewed);
+
+  useEffect(() => {
+    async function fetchPreviousReview() {
+      if (place) {
+        setIsLoading(true);
+        try {
+          const previousReview = await fetchReviewsByCafeId(place.place_id);
+          // Assuming fetchReviewsByCafeId returns the review content or null/undefined if not found
+          if (previousReview) {
+            setReview(previousReview);
+          }
+        } catch (error) {
+          console.error('Failed to fetch the review:', error);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    }
+
+    fetchPreviousReview();
+  }, [place]);
 
   async function handleReviewSubmit() {
     if (!session) {
@@ -31,13 +51,11 @@ const ReviewInputForm = ({
 
     if (!review) {
       setErrorMessage("Review can't be empty!");
-      // console.log(review);
       return;
     }
 
     setErrorMessage('');
 
-    // Check if cafe has already been reviewed, then update, else add
     if (alreadyReviewed) {
       updateReview(place!.place_id, review);
     } else {
@@ -48,6 +66,11 @@ const ReviewInputForm = ({
       setShowReviewFormOpen(false);
     }
   }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="flex flex-col text-primary-gray gap-3 items-center justify-center">
       <textarea
@@ -68,4 +91,77 @@ const ReviewInputForm = ({
     </div>
   );
 };
+
 export default ReviewInputForm;
+
+// 'use client';
+// import React, { useState, useContext } from 'react';
+// import { useSession } from 'next-auth/react';
+// import { Place } from '@/components/cafeFinder/cafefinder.component';
+// import { BookMarkPlace } from '@/app/bookmark/bookmarkCafe';
+// import { CafeContext } from '@/contexts/cafeContext';
+
+// const ReviewInputForm = ({
+//   place,
+//   setShowReviewFormOpen,
+// }: {
+//   place: Place | BookMarkPlace | null;
+//   setShowReviewFormOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+// }) => {
+//   const { data: session } = useSession();
+//   // console.log('/////data', data);
+//   const [review, setReview] = useState('');
+//   const [errorMessage, setErrorMessage] = useState('');
+//   const { isReviewed, fetchReviewsByCafeId, addReview, updateReview } =
+//     useContext(CafeContext);
+
+//   // Check if cafe is already reviewed
+//   const alreadyReviewed = isReviewed();
+//   // console.log('/////alreadyReviewed', alreadyReviewed);
+
+//   async function handleReviewSubmit() {
+//     if (!session) {
+//       console.log('User not authenticated');
+//       return;
+//     }
+
+//     if (!review) {
+//       setErrorMessage("Review can't be empty!");
+//       // console.log(review);
+//       return;
+//     }
+
+//     setErrorMessage('');
+
+//     // Check if cafe has already been reviewed, then update, else add
+//     if (alreadyReviewed) {
+//       updateReview(place!.place_id, review);
+//     } else {
+//       addReview(place!.place_id, review);
+//     }
+
+//     if (setShowReviewFormOpen) {
+//       setShowReviewFormOpen(false);
+//     }
+//   }
+//   return (
+//     <div className="flex flex-col text-primary-gray gap-3 items-center justify-center">
+//       <textarea
+//         className="rounded-lg text-sm w-full h-[6rem]"
+//         value={review}
+//         onChange={(e) => setReview(e.target.value)}
+//         placeholder="Write your review here..."
+//       />
+
+//       <button
+//         onClick={handleReviewSubmit}
+//         className="inline-flex justify-center rounded-md px-4 py-2 bg-primary-coral text-base font-medium text-white font-inter hover:bg-primary-rose focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#b9cbc6]">
+//         {alreadyReviewed ? 'Update Review' : 'Submit Review'}
+//       </button>
+//       {errorMessage && (
+//         <p className="text-sm font-medium text-[#DE1A17]">{errorMessage}</p>
+//       )}
+//     </div>
+//   );
+// };
+// export default ReviewInputForm;
