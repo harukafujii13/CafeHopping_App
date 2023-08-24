04255b51a356e6754cc0f7a8209c21135453fdcf
Haruka <hfujii1013@gmail.com>
2023-08-23 23:24:12 -0700

    delete: favorite

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/profile', '/main', '/favorite', '/bookmark'],
+  matcher: ['/profile', '/main', '/bookmark'],
 };
 
 // It can also be used to protect certain routes and redirect if a user isn't signed in.
